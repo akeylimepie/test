3776b959e05eb898f43131c0b1305b52feddb19a
Oleg Sedinkin <a@aw.wtf>
2024-04-19 02:09:51 +0500

    adsss

--- dasdsCommand.php
+++ dasdsCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class dasdsCommand extends Commandds
 dsddsa
-class dasdsCommand extends Command
+class dasdsCommand extends Commandss
 {
     /**
      * {@inheritdoc}
