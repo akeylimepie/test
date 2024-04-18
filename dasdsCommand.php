8ff71747f20ab296cc2d0909845d773de8f7d0e0
Oleg Sedinkin <a@aw.wtf>
2024-04-18 17:11:20 +0500

    i8

--- dasdsCommand.php
+++ dasdsCommand.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace;ds
 
+dsal
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
