aa207deb1f828c12d5b65185e8b2638296c8dbd9
Oleg Sedinkin <a@aw.wtf>
2024-04-18 15:29:26 +0500

    odna huinya

---
+++ dasdsCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class dasdsCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('app:dasds')
+            ->setDescription('Hello PhpStorm');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
