e0fc1fc885712cc542f7d2e9fa509706f1a1450b
Oleg Sedinkin <a@aw.wtf>
2024-04-18 18:01:32 +0500

    i11

--- dasdsCommand.php
+++ dasdsCommand.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace;ds
 
 
-fdsfd
-
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
