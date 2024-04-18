855658ad06ab679c388f810fe8e4bf33fef7d440
Oleg Sedinkin <a@aw.wtf>
2024-04-19 01:51:09 +0500

    adsss

--- dasdsCommand.php
+++ dasdsCommand.php
@@ -5,7 +5,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class dasdsCommand extends Commandds
-dsddsasss
+dsddsasssл
 class dasdsCommand extends Command
 {
     /**
